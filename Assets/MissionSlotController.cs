ec9dfba2f55be603ee8d480aa3743109f8957451
Jerry Choi <cyron1259@gmail.com>
2018-11-25 20:05:19 +0900

    Created Mission Slot Skeleton Script

---
+++ Assets/MissionSlotController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionSlotController : MonoBehaviour {
+
+    public Transform Spawnpoint;
+    public GameObject MissionSlotPrefab;
+
+    void SpawnMissionSlot()
+    {
+        Instantiate(MissionSlotPrefab, Spawnpoint.position, Spawnpoint.rotation);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
